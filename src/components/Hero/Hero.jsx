9e377505bbb369171c2f04971eaffb7afe2dcb3b
ramkumar-25 <ramkumar43540@gmail.com>
2024-09-13 22:09:04 +0530

    resume added

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import "./Hero.css"
 import profile_img from "../../assets/profile_img.jpg"
+import resume from "../../assets/Resume.pdf"
 
 const Hero = () => {
   return (
@@ -9,7 +10,7 @@ const Hero = () => {
         <h1>Hi I'm Ramkumar <span>Front End Developer</span></h1>
         <p>Working with my hands to make magic happen on the internet. View my <span>Projects</span> <span>resume</span> contact me or send me an email at ramkumar43540@gmail.com.</p>
         <p>Chennai,TamilNadu,India</p>
-        <button><a href="" download="resume">Resume</a></button>        
+        <button><a href={resume} download="resume">Resume</a></button>        
     </div>
   )
 }
